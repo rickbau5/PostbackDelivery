80223f58311ceb9bdc5af4bad62e088cca27648e
Rick Boss <rickeboss@gmail.com>
2016-03-02 14:33:37 -0800

    Implement continuous processing
    Test run of continuous processing, handling both POST and GET methods

--- src/github.com/rickbau5/deliveryagent/main.go
+++ src/github.com/rickbau5/deliveryagent/main.go
@@ -6,10 +6,11 @@ import (
     "gopkg.in/redis.v3"
     "io/ioutil"
     "net/http"
+    "net/url"
     "strings"
 )
 
-func newClient() *redis.Client {
+func redisClient() *redis.Client {
     client := redis.NewClient(&redis.Options{
         Addr: "127.0.0.1:6379",
         Password: "",
@@ -34,53 +35,70 @@ func jsonStringToMap(s string) map[string]interface{} {
     return requestData
 }
 
-func constructResponse(unformattedUrl string, dataMap map[string]interface{}) string {
+func constructGet(unformattedUrl string, dataMap map[string]interface{}) string {
     formatted := strings.Replace(unformattedUrl, "{key}", dataMap["key"].(string), 1)
     formatted = strings.Replace(formatted, "{value}", dataMap["value"].(string), 1)
     return formatted
 } 
 
-func sendResponse(response string, method string) {
+func sendResponse(uResponse string, dataMap map[string]interface{}, method string) {
     if method == "GET" {
-        resp, err := http.Get(response)
-        defer resp.Body.Close()
-        fmt.Println(method, resp, err)
-        body, err := ioutil.ReadAll(resp.Body)
-        fmt.Printf("%s\n", body)
+        response := constructGet(uResponse, dataMap)
+        if resp, respErr := http.Get(response); respErr == nil {
+            defer resp.Body.Close()
+            if body, err := ioutil.ReadAll(resp.Body); err == nil {
+                fmt.Printf("%s\n", body)
+            } else {
+                fmt.Println("Error reading body", err)
+            }
+        } else {
+            fmt.Println("Error with response", respErr)
+        }
     } else if method == "POST" {
-        fmt.Println("Gimme a minute or two.")
+        response := uResponse
+        if idx := strings.Index(response, "?"); idx != -1 {
+            response = response[:idx]
+        }
+        values := url.Values{"key":{dataMap["key"].(string)}, "value":{dataMap["value"].(string)}}
+        if resp, err := http.PostForm(response, values); err == nil{
+            defer resp.Body.Close()
+            body, _ := ioutil.ReadAll(resp.Body)
+            fmt.Printf("%s\n", body)
+        } else {
+            fmt.Println(err)
+        }
     } else {
         fmt.Println("Unknown method type", method)
     }    
 }
 
 func main() {
-    client := newClient()
+    client := redisClient()
     
     ping := client.Ping()
     if _, errPing := ping.Result(); errPing == nil {
-        popCmd := client.BRPop(0, "requests")
-        if popCmd.Err() == nil {
-            formatted := formatString(popCmd.String())
-            mapped := jsonStringToMap(formatted)
-            if end, ok := mapped["endpoint"]; ok {
-                endpoint := end.(map[string]interface{})
-                fmt.Println(endpoint)
-                if d, ok := mapped["data"]; ok {
-                    di := d.([]interface{})
-                    for _, data := range di {
-                        dataMap := data.(map[string]interface{})
-                        response := constructResponse(endpoint["url"].(string), dataMap)
-                        sendResponse(response, endpoint["method"].(string))
+        for {
+            popCmd := client.BRPop(0, "requests")
+            if popCmd.Err() == nil {
+                formatted := formatString(popCmd.String())
+                mapped := jsonStringToMap(formatted)
+                if end, ok := mapped["endpoint"]; ok {
+                    endpoint := end.(map[string]interface{})
+                    if d, ok := mapped["data"]; ok {
+                        di := d.([]interface{})
+                        for _, data := range di {
+                            dataMap := data.(map[string]interface{})
+                            sendResponse(endpoint["url"].(string), dataMap, endpoint["method"].(string))
+                        }
                     }
+                } else {
+                    fmt.Println("Endpoint is nil.")
                 }
             } else {
-                fmt.Println("Endpoint is nil.")
+                //on error
+                fmt.Println("Error while popping.")
+                fmt.Println(popCmd.Err())
             }
-        } else {
-            //on error
-            fmt.Println("Error while popping.")
-            fmt.Println(popCmd.Err())
         }
     } else {
         //Log error
