8d3b14165f9c72214a4225404a3f599aeaf4595f
Rick Boss <rickeboss@gmail.com>
2016-03-01 16:33:32 -0800

    Moved the logic from PHP to Go
    Rereading requirement doc made me realize logic should be in Go rather
    than in PHP

--- src/github.com/rickbau5/deliveryagent/main.go
+++ src/github.com/rickbau5/deliveryagent/main.go
@@ -3,6 +3,8 @@ package main
 import (
     "fmt"
     "gopkg.in/redis.v3"
+    "encoding/json"
+    "strings"
 )
 
 func newClient() *redis.Client {
@@ -14,11 +16,37 @@ func newClient() *redis.Client {
     return client
 }
 
+func formatString(s string) string {
+    t := strings.Trim(s, "]")
+    formatted := strings.Replace(t, "BRPOP requests 0: [requests ", "", 1)
+    return formatted
+}
+
 func main() {
-    fmt.Println("Hello world")
     client := newClient()
     
-    pong, err := client.Ping().Result()
-    fmt.Println(pong, err)
+    ping := client.Ping()
+    if _, errPing := ping.Result(); errPing == nil {
+        popCmd := client.BRPop(0, "requests")
+        if popCmd.Err() == nil {
+            formatted := formatString(popCmd.String())
+            var requestData map[string]interface{}
+            converted := []byte(formatted)
+            jsonErr := json.Unmarshal(converted, &requestData)
+            if jsonErr == nil {
+                fmt.Println(requestData)
+            } else {
+                fmt.Println("Couldn't parse JSON :( ")
+                fmt.Println(jsonErr)
+            }
+        } else {
+            //on error
+            fmt.Println("Error while popping.")
+            fmt.Println(popCmd.Err())
+        }
+    } else {
+        //Log error
+        fmt.Println(errPing)
+    }
 }
 
--- src/ingest.php
+++ src/ingest.php
@@ -13,23 +13,7 @@ if ($decoded) {
 
     if ($connected) {
         if (isset($decoded['data']) && isset($decoded['endpoint'])) {
-            // Verify this
-            $endpointURL = $decoded['endpoint']['url'];
-            $endpointMethod = $decoded['endpoint']['method'];
-
-            foreach($decoded['data'] as &$data) {
-                $key = 'key';
-                $value = 'value';
-
-                if (isset($data[$key]) && isset($data[$value])) {
-                    $keyed = str_replace(braced($key), $data[$key], $endpointURL);
-                    $populated = str_replace(braced($value), $data[$value], $keyed);
-
-                    $redis->lPush('requests', $populated);
-                } else {
-                    echo 'Malformed data object.' . PHP_EOL;
-                }
-            }
+            $redis->lPush('requests', $postdata);
         } else {
             echo 'No data received.' . PHP_EOL;
         }
